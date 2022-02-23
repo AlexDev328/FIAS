9c6b583409970dd965ff7519491ec75e032b65bb
azamyslov <azamyslov@moysklad.ru>
2022-02-23 22:17:59 +0300

    added additional parsers for fias objects

--- app/update_manager/parser/base_parser.py
+++ app/update_manager/parser/base_parser.py
@@ -1,7 +1,7 @@
 from _elementtree import Element
 from typing import Generic, TypeVar, Type
 
-from app.update_manager.parser.data_classes import FiasObject, House, AbstractFiasObject
+from app.update_manager.parser.data_classes import *
 
 T = TypeVar("T", bound=AbstractFiasObject)
 
@@ -30,3 +30,21 @@ class HouseParser(BaseParser[House]):
         super().__init__(House)
 
 
+class SteadParser(BaseParser[Stead]):
+    def __init__(self):
+        super().__init__(Stead)
+
+
+class RoomParser(BaseParser[Room]):
+    def __init__(self):
+        super().__init__(Room)
+
+
+class ApartmentParser(BaseParser[Apartment]):
+    def __init__(self):
+        super().__init__(Apartment)
+
+
+class CarPlaceParser(BaseParser[CarPlace]):
+    def __init__(self):
+        super().__init__(CarPlace)
